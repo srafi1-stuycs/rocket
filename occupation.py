26ebe51c2e736a636053b8543f6582b2c0f9e1f3
aroktim@stuy.edu <aroktim@stuy.edu>
2017-09-15 11:58:03 -0400

    Did code that returns random occupation

--- occupation.py
+++ occupation.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 occupation_dict = {}
 
@@ -6,6 +7,24 @@ def read_csv():
     with open('occupations.csv', 'rU') as f:
         reader = csv.reader(f, delimiter=",", quotechar='"')
         for row in reader:
-            occupation_dict[row[0]] = row[1]
+            if row[0] == "Job Class":
+                continue
+            occupation_dict[row[0]] = float (row[1])
+
+def ret_rand():
+    keys = occupation_dict.keys()
+    rand = random.random() * occupation_dict['Total']
+    #print rand
+    for key in keys:
+        if key == "Total":
+            continue
+        rand -= occupation_dict[key]
+        if rand <= 0:
+            return key
+    return 0;
+
+
 read_csv()
 print occupation_dict
+
+print ret_rand()
